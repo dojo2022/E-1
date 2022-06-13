46b5294c9fa97ad9c05d5aa007a81af65b789057
dojo2022-tanakaryo <103239418+dojo2022-tanakaryo@users.noreply.github.com>
2022-06-13 17:55:30 +0900

    Mypage関係ファイルの更新

--- src/WebContent/WEB-INF/jsp/mypage.jsp
+++ src/WebContent/WEB-INF/jsp/mypage.jsp
@@ -4,9 +4,46 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>マイページ</title>
+<link rel= "stylesheet" type="text/css" href ="/dokogacha/css/common.css">
+<link rel= "stylesheet" type="text/css" href ="/dokogacha/css/mypage.css">
+<!---->
+<style>*{outline: 1px solid #ff0000;}</style>
 </head>
 <body>
+<!-- ナビゲーションバー -->
+
+<!-- メイン -->
+<!-- ユーザアイコン、ユーザ名、お気に入り投稿リンクの表示 -->
+<a href="/dokogacha/LogoutServlet"><img src="/dokogacha/img/cap_logout.png" alt="ログアウト" width=100px class="capsule_logout" ></a>
+<div class ="user_inf1">
+	<div class ="user_image	">
+		<img src="/dokogacha/img/icon_panda.png" alt="ユーザアイコン" width=100px >
+	</div>
+	<div class ="user_name_title">
+		<span class ="user_name">ユーザ―ネーム</span>
+		 <img src="/dokogacha/img/shiro_panda.png" alt="称号" width=100px class="title"><br>
+		 <a href="/dokogacha/FavoriteReviewListServlet" class="favorite_review_list">お気に入り投稿一覧</a>
+	</div>
+</div>
+
+<!-- 累計いいね数とお気に入りジャンルの表示 -->
+<div class="user_inf2">
+	<div class="total_good">累積いいね数 <!--ページスコープで持ってくる？--></div>
+	<div class="Favorite_Genre">お気に入りジャンル<br></div><!-- 繰り返しN文字異常なら折り返し -->
+</div>
+
+<!-- 編集ボタンと自分の最新投稿、自分の投稿一覧リンクの表示 -->
+<div class="user_inf3">
+	<div class="do_change"><a href="/dokogacha/MypageChangeServlet">
+			<img src="/dokogacha/img/button_edit.png" alt="編集" width=100px class="botton_edit" ></a></div>
+	<div class="my_new_review">＃ジャンル<br>金額　商品名　<br>♡いいね数　<br>
+	<a href="/dokogacha/MyReviewListServlet">自分の投稿一覧へ</a>
+	</div>
+</div>
+
+
+<!-- フッター -->
 
 </body>
 </html>
---
+++ src/src/servlet/MypageServlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MypageServlet
+ * マイページへの遷移　LoginUserのUser_Idを入手し、ログイン状態を判定
+ * ログインしていなければログインページへリダイレクトさせる
+ *
+ */
+@WebServlet("/MypageServlet")
+public class MypageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		/*
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/dokogacha/LoginServlet");
+			return;
+		}
+		else {
+		}
+		*/
+		doPost( request , response);
+		return;
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		//Userテーブルからuser_Idに該当するレコードを取得
+
+		//
+
+		// メニューページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypage.jsp");
+		dispatcher.forward(request, response);
+
+	}
+}
