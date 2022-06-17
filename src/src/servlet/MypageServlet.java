b84d9983d49bb145c4b06ca0aa467be263d8e6c3
dojo2022-tanakaryo <103239418+dojo2022-tanakaryo@users.noreply.github.com>
2022-06-17 15:38:55 +0900

    Update MypageServlet.java

--- src/src/servlet/MypageServlet.java
+++ src/src/servlet/MypageServlet.java
@@ -21,9 +21,11 @@ public class MypageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
 		// マイメニュー画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypage.jsp");
 		dispatcher.forward(request, response);
