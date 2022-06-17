eb81d32ea5130be8b99a6e8a7c4f08c74d648757
dojo2022-hirano <103238464+dojo2022-hirano@users.noreply.github.com>
2022-06-17 13:35:08 +0900

    update

--- src/src/servlet/ReviewDetailServlet.java
+++ src/src/servlet/ReviewDetailServlet.java
@@ -19,9 +19,36 @@ public class ReviewDetailServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/dokogacha/LoginServlet");
+			return;
+		}*/
+			ReviewDao iDao = new ReviewDao();
+		List<Review> review_detailList = iDao.select(new Review(review_id));
+		request.setAttribute("review_detailList", review_detailList);
+
 		// レビュー詳細ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/review_detail.jsp");
 		dispatcher.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/dokogacha/LoginServlet");
+			return;
+		}*/
+		request.setCharacterEncoding("UTF-8");
+		String review_id = request.getParameter("${e.review_id}");
+
+		ReviewDao FRDao = new ReviewDao();
+		if (request.getParameter("goodbutton").equals("いいねボタン")) {
+			 FRDao.goodcount(review_id) ;
+
+		} else if (request.getParameter("favoritebutton").equals("お気に入りボタン")){
+			FRDao.favoriteregist(review_id) ;
+		}
 	}
 }
