1e31981d9287bec66549a47800aaab066f9cf50f
dojo2022-tanakaryo <103239418+dojo2022-tanakaryo@users.noreply.github.com>
2022-06-20 17:58:42 +0900

    Create testUserDao.java

---
+++ src/src/test/testUserDao.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.util.List;
+
+import dao.UserDao;
+import model.User;
+
+public class testUserDao {
+	public static void main(String[] args) {
+	UserDao dao = new UserDao();
+//----------------------select()のテスト---------------------------------------------
+	System.out.println("---------- select()のテスト ----------");
+	List<User> recordList2 = dao.select("");
+	for (User record : recordList2) {
+		System.out.println("　id：" + record.getId());
+		System.out.println("　User_image：" + record.getImage());
+		System.out.println("　public：" + record.getC_public());
+		System.out.println();
+		}
+//----------------------Insert()のテスト---------------------------------------------
+	///*
+	System.out.println("---------- Insert()のテスト ----------");
+	if (dao.insert("username", "pass")){
+		System.out.println("登録成功！");
+		List<User> recordList3 = dao.select("");
+		for (User record : recordList3) {
+			System.out.println("　id：" + record.getId());
+			System.out.println("　User_image：" + record.getImage());
+			System.out.println("　public：" + record.getC_public());
+			System.out.println();
+			}
+	}
+	else {
+		System.out.println("登録失敗！");
+	}
+	//*/
+	//----------------------update()のテスト---------------------------------------------
+		///*
+			System.out.println("---------- update()のテスト ----------");
+			if (dao.update("username", "tatataki", "pass", "no")){
+			System.out.println("更新成功！");
+			List<User> recordList3 = dao.select("");
+			for (User record : recordList3) {
+				System.out.println("　id：" + record.getId());
+				System.out.println("　User_image：" + record.getImage());
+				System.out.println("　public：" + record.getC_public());
+				System.out.println();
+				}
+		}
+		else {
+			System.out.println("更新失敗！");
+		}
+		//*/
+//-----------------------------------------------------------------------------------
+	}
+}
