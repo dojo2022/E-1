2eab8180206401eaf7eaff6623b827bb75496f55
dojo2022-tanakaryo <103239418+dojo2022-tanakaryo@users.noreply.github.com>
2022-06-21 12:52:17 +0900

    Update TestReviewDao.java

--- src/src/test/TestReviewDao.java
+++ src/src/test/TestReviewDao.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.List;
+
 import dao.ReviewDao;
 import model.Review;
 
@@ -8,15 +10,24 @@ public class TestReviewDao {
 		ReviewDao rDao = new ReviewDao();
 		if (rDao.insert(new Review(0, "user_name", 1, "review_day", "title", "series", "thought", 1, 0, "address", "product_name", 100))){
 			System.out.println("登録成功！");
-			/*
-			List<User> recordList3 = rDao.select("");
-			for (User record : recordList3) {
-				System.out.println("　id：" + record.getId());
-				System.out.println("　User_image：" + record.getImage());
-				System.out.println("　public：" + record.getC_public());
+			List<Review> ReviewList1 = rDao.allselect();
+			for (Review review : ReviewList1) {
+				System.out.println("　id：" + review.getReview_id());
+				System.out.println("　User_name：" + review.getUser_name());
+				System.out.println("　Genre_id：" + review.getGenre_id());
+				System.out.println("　Review_day：" + review.getReview_id());
+				System.out.println("　Title：" + review.getTitle());
+				System.out.println("　Series_name：" + review.getSeries_name());
+				System.out.println("　Thought：" + review.getThought());
+				System.out.println("　Evalution：" + review.getEvalution());
+				System.out.println("　Good：" + review.getGood());
+				System.out.println("　Address：" + review.getAddress());
+				System.out.println("　Product_name：" + review.getProduct_name());
+				System.out.println("　Price：" + review.getPrice());
+
 				System.out.println();
 				}
-			*/
+
 		}
 		else {
 			System.out.println("登録失敗！");
