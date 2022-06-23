7337a7e22a514cded058146136c8d16e07ea86bb
dojo2022-Nakashima-Yuki <103239320+dojo2022-Nakashima-Yuki@users.noreply.github.com>
2022-06-23 13:46:24 +0900

    s

--- src/src/dao/ReviewDao.java
+++ src/src/dao/ReviewDao.java
@@ -169,7 +169,7 @@ public class ReviewDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/data/dokogacha", "sa", "");
 
 			String sql = "select review.review_id , genre_name , price , puroduct_name , good , image "
-					+ "from review join review_image on review.review_id = review_image.review_id"
+					+ "from review join review_image on review.review_id = review_image.review_id "
 					+ "right join genre on review.genre_id = genre.genre_id WHERE user_name = ?";//変更部分
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
