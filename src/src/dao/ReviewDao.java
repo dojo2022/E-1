4a9b5207e8747e0fe054a2aaf7d745c2b3b23d57
dojo2022-kashimori <103237598+dojo2022-kashimori@users.noreply.github.com>
2022-06-23 14:59:37 +0900

    Update ReviewDao.java

--- src/src/dao/ReviewDao.java
+++ src/src/dao/ReviewDao.java
@@ -437,6 +437,68 @@ public class ReviewDao {
 		// 結果を返す
 		return reviewList;
 	}
+
+//------------------------------------------------------------------------------------------
+//トレンドを呼び出す
+public List<Review_List> TRselect(Review_List review_id) {
+	/*public List<Review> select(Stirng genre, ){*/ //バラバラに呼び出すやり方
+
+	Connection conn = null;
+	List<Review_List> reviewList = new ArrayList<Review_List>();
+
+
+	try {
+		Class.forName("org.h2.Driver");
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/data/dokogacha", "sa", "");
+
+		String sql = "SELECT MAX(good=?) from review WHERE date < DATE_SUB(now(), INTERVAL 1 month)";//変更部分
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+		int id = review_id.getReview_id();//変更部分
+
+
+		pStmt.setInt(1, id);//変更部分
+
+
+		// SQL文を実行し、結果表を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		// 結果表をコレクションにコピーする<<ここ改造>>//変更部分
+		while (rs.next()) {
+			Review_List review  = new Review_List(
+					rs.getInt("review_id"),
+					rs.getString("image"),
+					rs.getString("genre_name"),
+					rs.getInt("price"),
+					rs.getString("puroduct_name"),
+					rs.getInt("good")
+					);
+			reviewList.add(review);
+		}
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+		reviewList = null;
+	}
+	catch (ClassNotFoundException e) {
+		e.printStackTrace();
+		reviewList = null;
+	}
+	finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				reviewList = null;
+			}
+		}
+	}
+
+	// 結果を返す
+	return reviewList;
+  }
 }
 
 
