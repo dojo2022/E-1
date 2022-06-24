224294876a4be48fc80e3ac95f44421131308ee7
dojo2022-kashimori <103237598+dojo2022-kashimori@users.noreply.github.com>
2022-06-24 15:15:55 +0900

    a

--- src/src/dao/ReviewDao.java
+++ src/src/dao/ReviewDao.java
@@ -543,7 +543,7 @@ public List<Review_List> Tselect(int review_id) {
 
 		String sql = "select review.review_id, genre_name , price , puroduct_name , good , image "
 				+ "from review join review_image on review.review_id = review_image.review_id "
-				+ "right join genre on review.genre_id = genre.genre_id WHERE review_id = ?";//変更部分
+				+ "right join genre on review.genre_id = genre.genre_id WHERE review.review_id = ?";//変更部分
 		PreparedStatement pStmt = conn.prepareStatement(sql);
 
 		pStmt.setInt(1, review_id);//変更部分
