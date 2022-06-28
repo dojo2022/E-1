295e4035d41a5f967d904bbdc929ece2b5274226
dojo2022-tanakaryo <103239418+dojo2022-tanakaryo@users.noreply.github.com>
2022-06-28 12:13:04 +0900

    Update ReviewServlet.java

--- src/src/servlet/ReviewServlet.java
+++ src/src/servlet/ReviewServlet.java
@@ -2,15 +2,18 @@ package servlet;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import dao.ReviewDao;
 import dao.Review_ImageDao;
@@ -18,6 +21,9 @@ import model.LoginUser;
 import model.Review;
 import model.Review_Image;
 
+
+//↓アップロードファイルの一時的な保存先
+@MultipartConfig(location = "C:/dojo6/src/WebContent/img/review_image")
 @WebServlet("/ReviewServlet")
 public class ReviewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -42,16 +48,20 @@ public class ReviewServlet extends HttpServlet {
 		int star;
 		String star_Str = request.getParameter("star");
 		if( star_Str== null|| star_Str == "") {
-			star = 0;
+			star = 1;
 		}else {
 			star = Integer.parseInt(request.getParameter("star"));
 		}
 		//金額が入力されていない場合の処理
-		int price;
+		int price = 0;
 		String price_Str = request.getParameter("price");
-		if(price_Str == null || price_Str == "") {
+		if( price_Str.equals("")) {
 			price = 0;
-		}else {
+		}else if(!price_Str.chars().allMatch(Character::isDigit)){
+			request.setAttribute("error_message","※金額は数値のみ入力してください。");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/review.jsp");
+			dispatcher.forward(request, response);
+		}		else {
 			price = Integer.parseInt(request.getParameter("price"));
 		}
 
@@ -64,40 +74,57 @@ public class ReviewServlet extends HttpServlet {
 		String series = request.getParameter("series");
 		String thought = request.getParameter("thought");
 		String address = request.getParameter("address");
-		String image = request.getParameter("insert_image");
-		String image2 = request.getParameter("insert_image");
+		//String image = request.getParameter("insert_image");
 		ReviewDao rDao = new ReviewDao();
 		Review_ImageDao riDao = new Review_ImageDao();
-
-		System.out.println(image);
-		System.out.println(image2);
 
 		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date now = new Date();
 		String review_day = f.format(now);
 
 
+		//画像関係の処理
+		Part part = request.getPart("insert_image");
+		String image = this.getFileName(part);
+
+		String[] images = new String[4];
+
+
+		Collection<Part> parts = request.getParts();
+
+		int i = 0;
+		for(Part part2 : parts) {
+			if(part2.getName().equals("insert_image")) {
+				images[i] = this.getFileName(part2);
+				i++;
+			}
+		}
+
 
 		//金額に文字が入力されていた場合のエラー処理
-		if(!price_Str.chars().allMatch(Character::isDigit)){
-			request.setAttribute("error_message","※金額は数値のみ入力してください。");
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/review.jsp");
-			dispatcher.forward(request, response);
-
-		}
 		//正常に動作した場合の処理
-		else {
-			if(rDao.insert(new Review(0,user_name, genre_id, review_day, title, series, thought, star, good, address, product_name, price))) {
-				int rev_id = rDao.rev_id();
-				System.out.println(rev_id);
-				if(riDao.insert_image(new Review_Image(rev_id,image))) {
-					response.sendRedirect("/dokogacha/ReviewResultServlet");
-					return;
+		if(rDao.insert(new Review(0,user_name, genre_id, review_day, title, series, thought, star, good, address, product_name, price))){
+			int rev_id = rDao.rev_id();
+			System.out.println(rev_id);
+			if(riDao.insert_image(new Review_Image(rev_id,image))) {
+				response.sendRedirect("/dokogacha/ReviewResultServlet");
+				return;
 				}
-			}
 			//rDaoにて、review_imageを追加
 		}
 	}
-
+/*----------------------------------------------------------------------------------------------*/
+//ファイルの名前を取得してくる　パスはここで分離される。
+	private String getFileName(Part part) {
+        String name = null;
+        for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+            if (dispotion.trim().startsWith("filename")) {
+                name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+                name = name.substring(name.lastIndexOf("\\") + 1);
+                break;
+            }
+        }
+		return name;
+	}
 
 }
