20d37bc728c543ccb6f79fe480c5f0050058fdfa
dojo2022-tanakaryo <103239418+dojo2022-tanakaryo@users.noreply.github.com>
2022-06-29 10:43:41 +0900

    Update mypage.js

--- src/WebContent/js/mypage.js
+++ src/WebContent/js/mypage.js
@@ -1,3 +1,12 @@
-/**
- * 
- */
+function submit_edit(){
+	var rotate = document.querySelector('.rotate');
+	rotate.animate(
+		[
+		{ transform: 'rotate(0deg)' },
+		{ transform: 'rotate(360deg)' }
+		],
+		{ duration: 3000,
+		  easing: 'linear'
+		}
+			);
+}
